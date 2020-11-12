d41dc175229416a97e36d0ff9018ec8206543adf
JaydeepMor <it.jaydeep.mor@gmail.com>
2020-11-12 21:08:13 +0530

    Revert temp.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,18 @@ class AppServiceProvider extends ServiceProvider
         date_default_timezone_set('UTC');
 
         // Load default constants.
-        
+        $constants = Constant::all();
+
+        if (!empty($constants) && !$constants->isEmpty()) {
+            foreach ($constants as $constant) {
+                if (empty($constant->key) || empty($constant->value)) {
+                    continue;
+                }
+
+                if (!defined(strtoupper($constant->key))) {
+                    define(strtoupper($constant->key), $constant->value);
+                }
+            }
+        }
     }
 }
