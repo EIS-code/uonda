0ac311060c2d915afb21e9c6c0194679819a866f
Mihir Patel <mihir.95patel@gmail.com>
2020-11-29 11:17:10 +0530

    Integrated the admin theme, Made changes to login only admin users and other restrictions.

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes(['register' => false, 'reset' => false]);
+
+Route::group(['middleware' => ['auth']], function(){
+    Route::get('/', 'DashboardController@index')->name('dashboard');
 });
 
-Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
