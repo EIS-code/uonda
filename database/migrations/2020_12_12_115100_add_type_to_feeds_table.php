385faccbeddab13b09698642fb4eb0ad07bcb0df
JaydeepMor <it.jaydeep.mor@gmail.com>
2020-12-15 14:09:18 +0530

    Add feed type migration.

--- database/migrations/2020_12_12_115100_add_type_to_feeds_table.php
+++ database/migrations/2020_12_12_115100_add_type_to_feeds_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddTypeFieldInFeedsTable extends Migration
+class AddTypeToFeedsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddTypeFieldInFeedsTable extends Migration
     public function up()
     {
         Schema::table('feeds', function (Blueprint $table) {
-            $table->tinyInteger('type')->nullable()->comment('1=Image, 2=Video, 3=GIF')->after('sub_title');
+            $table->enum('type', ['0', '1', '2', '3', '4'])->default('0')->comment('0: Null, 1: Image, 2: URL, 3: Video, 4: GIF')->after('description');
         });
     }
 
