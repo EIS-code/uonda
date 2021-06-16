4ac9bb4d31a4a480b4275952fc95ec2e4eb13ba7
JaydeepMor <it.jaydeep.mor@gmail.com>
2021-06-16 12:12:32 +0530

    Add superadmin seeder.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(LocationSeeder::class);
-        // $this->call(ConstantsSeeder::class);
-        // $this->call(FeedsSeeder::class);
-        // $this->call(UserReportQuestionsSeeder::class);
-        // $this->call(ImageSeeder::class);
-        $this->call(DummyUsersSeeder::class);
+        $this->call(ConstantsSeeder::class);
+        $this->call(FeedsSeeder::class);
+        $this->call(UserReportQuestionsSeeder::class);
+        $this->call(ImageSeeder::class);
+        $this->call(SuperadminUserSeeder::class);
+        // $this->call(DummyUsersSeeder::class);
     }
 }
