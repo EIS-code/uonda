53ecf43fb5021f5687ca9e150f88e23ed722853c
JaydeepMor <it.jaydeep.mor@gmail.com>
2021-06-25 12:29:20 +0530

    Debug code.

--- app/Http/Controllers/User/Chat/ChatController.php
+++ app/Http/Controllers/User/Chat/ChatController.php
@@ -909,7 +909,7 @@ class ChatController extends BaseController
                     $userRow->Users->setHidden(['encrypted_user_id', 'permissions', 'total_notifications', 'total_read_notifications', 'total_unread_notifications']);
                 });
 
-                $row->chat_room_users = array_values($chatRoomUsers->toArray());
+                $chatRoomDetails[$index]->chatRoomUsers = array_values($row->chatRoomUsers->toArray());
             });
         }
 
