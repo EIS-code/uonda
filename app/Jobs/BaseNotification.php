a70daba7495dfa317039b9dc5f213200618a5074
Jaydeep Mor <it.jaydeep.mor@gmail.com>
2021-07-29 16:58:22 +0530

    debug

--- app/Jobs/BaseNotification.php
+++ app/Jobs/BaseNotification.php
@@ -71,6 +71,8 @@ class BaseNotification
         }
 
         $this->title = $isScreenShot ? $this->user->full_name . ' ' . $title : $title;
+
+        dd(uniqueValues($this->deviceTokens));
     }
 
     public function getUser()
