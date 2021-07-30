27f209b7be42f3fd8236627d90aab33482a36e40
Jaydeep Mor <it.jaydeep.mor@gmail.com>
2021-07-30 12:28:48 +0530

    Fix mixed content https issue.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,11 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <!-- <meta http-equiv="Content-Security-Policy" content="block-all-mixed-content"> -->
+    @if (env('APP_ENV') == 'production' && env('APP_PROTOCOL') == 'https')
+        <meta http-equiv="Content-Security-Policy" content="block-all-mixed-content">
+    @else
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    @endif
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
