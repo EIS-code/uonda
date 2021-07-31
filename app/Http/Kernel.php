eec95014ca2877b7247103f42a0f0b5efd145f1b
Jaydeep Mor <it.jaydeep.mor@gmail.com>
2021-07-31 11:38:04 +0530

    Fix CORS policy by adding middleware.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'web.auth.api' => \App\Http\Middleware\AuthApi::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
