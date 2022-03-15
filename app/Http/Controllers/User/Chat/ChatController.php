643300dfa10ac0f41bfeaa0c46ba10db98025af0
Jaydeep Mor <it.jaydeep.mor@gmail.com>
2022-03-15 11:26:37 +0530

    Debugging

--- app/Http/Controllers/User/Chat/ChatController.php
+++ app/Http/Controllers/User/Chat/ChatController.php
@@ -528,7 +528,7 @@ class ChatController extends BaseController
             $dbReceiverId              = collect($records)->pluck('receiver_id');
 
             // Get receiver user.
-            $receiverUsers = $model::whereIn('id', $dbReceiverId)->get()->pluck('id');
+            $receiverUsers = $model::select('profile', 'profile_icon')->whereIn('id', $dbReceiverId)->get()->keyBy('id');
             dd($receiverUsers);
 
             foreach ($records as &$record) {
