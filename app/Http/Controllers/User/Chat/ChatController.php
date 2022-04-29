2a613c6a0ae2004d33a80a4998eeb721399ea075
Jaydeep Mor <it.jaydeep.mor@gmail.com>
2022-04-29 15:27:48 +0530

    Fixing

--- app/Http/Controllers/User/Chat/ChatController.php
+++ app/Http/Controllers/User/Chat/ChatController.php
@@ -634,7 +634,7 @@ class ChatController extends BaseController
             $delete       = [];
 
             // Remove chats from Chat table.
-            $modelChats::whereIn('id', $chatIds)->remove();
+            $modelChats::whereIn('id', $chatIds->toArray())->remove();
 
             foreach ($chatIds as $index => $chatId) {
                 $delete[$index] = [
