88b8f53947d2294fb92a9c12c6aac4fa233cdea4
Jaydeep Mor <it.jaydeep.mor@gmail.com>
2022-05-19 12:54:07 +0530

    Fixing

--- config/fcm.php
+++ config/fcm.php
@@ -5,7 +5,7 @@ return [
     'log_enabled' => false,
 
     'http' => [
-        'server_key' => env('AAAA6rPuhHs:APA91bHqkn3tf1fiZ0iyOrpB9l88sngOsMMaR92UFXfoYgOWgMRS_oPHR2BGIf6ncgYh3qIg-RvxoHKIjJAX8_qSauYq9Z8gfu_wrr53nF9N7yGQES7l92ZzvznbeYhbhG7oqN0yjtEy', ''),
+        'server_key' => 'AAAA6rPuhHs:APA91bHqkn3tf1fiZ0iyOrpB9l88sngOsMMaR92UFXfoYgOWgMRS_oPHR2BGIf6ncgYh3qIg-RvxoHKIjJAX8_qSauYq9Z8gfu_wrr53nF9N7yGQES7l92ZzvznbeYhbhG7oqN0yjtEy',
         'sender_id' => env('FCM_SENDER_ID', ''),
         'server_send_url' => 'https://fcm.googleapis.com/fcm/send',
         'server_group_url' => 'https://android.googleapis.com/gcm/notification',
